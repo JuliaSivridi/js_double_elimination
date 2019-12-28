b0e555c294bd5266946b6eea398858a6412d2497
UliaSivridi <58641003+UliaSivridi@users.noreply.github.com>
2019-12-28 15:17:02 +0300

    init button removed
    no needs in players list - add button adding players straight in tournament

--- double_elimination.js
+++ double_elimination.js
@@ -1,35 +1,22 @@
-var players = [];
 class Player {
-  constructor(name, losses) {
-    this.name = name;
-	this.losses = losses;
-  }
+	constructor(name) {
+		this.name = name;
+		this.losses = 0;
+		this.inMatch = false;
+	}
 }
 
-function addPlayer() {
-	var playerName = document.getElementById("playerName").value;
-	if (!playerName) return;
-	document.getElementById("playerName").value = "";
-	players.push(new Player(playerName, 0));
-	$("#playerList").append(playerName + ', ');
-}
-
-function shuffle(array) {
-  array.sort(() => Math.random() - 0.5);
-}
-
-var matches = [];
 class Match {
-  constructor() {
-	this.players = [];
-	this.isPlayed = false;
-  }
+	constructor() {
+		this.players = [];
+		this.gridMatch = undefined;
+	}
 }
 
-function setMatchType(matchIndex, matchType) {
-	if (matches[matchIndex].isWinMatch == undefined) {
-		matches[matchIndex].isWinMatch = matchType;
-		var ltr = matchType === true ? 'w' : 'l';
+function setMatchGrid(matchIndex, gridMatch) {
+	if (matches[matchIndex].gridMatch == undefined) {
+		matches[matchIndex].gridMatch = gridMatch;
+		var ltr = gridMatch ? 'w' : 'l';
 		$("#match_" + matchIndex).append(ltr + '&nbsp;');
 	}
 }
@@ -39,136 +26,73 @@ function makeBtn(name, matchIndex, playerIndex) {
 		text: '[ ' + name + ' ]',
 		id: 'btn_' + matchIndex + '_' + playerIndex,
 		click: playMatch.bind(this, matchIndex, playerIndex)
-	});
-	$("#match_" + matchIndex).append(btn);		
+	}); $("#match_" + matchIndex).append(btn);		
 }
 
-function isFreePlace(matchIndex){
-	var isFree = !matches[matchIndex].players[0] || !matches[matchIndex].players[1];
-	return isFree;
-}
-
-function howManyAlive() {
-	counter = 0;
-	for (i = 0; i < players.length; i++) {
-		if (players[i].losses < 2) counter++;
-	}
-	return counter;
-}
-
+var players = []; var matches = [];
 function initTour() {
-	shuffle(players);
-	var playerCount = players.length;
-	var matchCount = 2 * playerCount - 1;
-	for (var m = 0; m < matchCount; m++) {
+	var playerName = document.getElementById("playerName").value;
+	if (!playerName) return;
+	document.getElementById("playerName").value = "";
+	var p = players.push(new Player(playerName, 0)) - 1;
+	for (var m = 0; m < (2 * players.length - 1); m++) {
 		if (matches[m] == undefined) {
 			matches[m] = new Match();
 			$("#matches").append('<div id="match_' + m + '" class="notPlayed">#' + m + '&nbsp;</div>');
 		}
-	}
-	for (var p = 0; p < playerCount; p++) {
-		for (var m = 0; m < matches.length; m++) {
-			if (isFreePlace(m)) {
-				var playerIndex = matches[m].players[0] == undefined ? 0 : 1;
-				if (players[p].inMatch == undefined) {
-					if (playerIndex == 0) setMatchType(m, true);
-					matches[m].players[playerIndex] = players[p];
-					players[p].inMatch = true;
-					makeBtn(players[p].name, m, playerIndex);
-				}
+		if ((matches[m].gridMatch == undefined || matches[m].gridMatch == true) 
+			&& (!matches[m].players[0] || !matches[m].players[1])) {
+			var playerIndex = matches[m].players[0] == undefined ? 0 : 1;
+			if (players[p].inMatch === false) {
+				if (playerIndex == 0) setMatchGrid(m, true);
+				matches[m].players[playerIndex] = players[p];
+				players[p].inMatch = true;
+				makeBtn(players[p].name, m, playerIndex);
 			}
 		}
 	}
 }
 
+function movePlayer(gridMatch, player) {
+	for (var m = 0; m < matches.length; m++) {
+		setMatchGrid(m, gridMatch);
+		if ((matches[m].gridMatch === gridMatch) && (!matches[m].players[0] || !matches[m].players[1])) {
+			var playerIndex = matches[m].players[0] == undefined ? 0 : 1;
+			matches[m].players[playerIndex] = player;
+			makeBtn(player.name, m, playerIndex);
+			break;
+		}
+	}
+}
+
+function howManyAlive() {
+	counter = 0;
+	for (p = 0; p < players.length; p++) {
+		if (players[p].losses < 2) counter++;
+	} return counter;
+}
+
 function playMatch(matchIndex, winnerIndex) {
-	matches[matchIndex].isPlayed = true;
 	$("#match_" + matchIndex).removeClass("notPlayed");
 	$("#match_" + matchIndex).addClass("wasPlayed");
-
 	var winner = matches[matchIndex].players[winnerIndex];
 	var loserIndex = winnerIndex == 0 ? 1 : 0;
 	var loser = matches[matchIndex].players[loserIndex];
 	loser.losses++;
 	$("#btn_" + matchIndex + '_' + loserIndex).addClass("loser");
-
+	$("#btn_" + matchIndex + '_' + winnerIndex).unbind('click');
+	$("#btn_" + matchIndex + '_' + loserIndex).unbind('click');
 	if (howManyAlive() < 2) {
 		$("#btn_" + matchIndex + '_' + winnerIndex).addClass("totalwinner");
-		$("#btn_" + matchIndex + '_' + winnerIndex).unbind('click');
-		$("#btn_" + matchIndex + '_' + loserIndex).unbind('click');
 		$("#match_" + (matches.length-1)).addClass("wasPlayed");
 	} else {
 		$("#btn_" + matchIndex + '_' + winnerIndex).addClass("winner");
-
-		if (loser.losses < 2) {
-			if (howManyAlive() < 3) {
-				// move loser to winners in superfinal
-				for (var m = 0; m < matches.length; m++) {
-					setMatchType(m, true);
-					if (matches[m].isWinMatch === true) {
-						if (isFreePlace(m)) {
-							var playerIndex = matches[m].players[0] == undefined ? 0 : 1;
-							matches[m].players[playerIndex] = loser;
-							makeBtn(loser.name, m, playerIndex);
-							break;
-						}
-					}
-				}
-			} else {
-				// move loser to losers
-				for (var m = 0; m < matches.length; m++) {
-					setMatchType(m, false);
-					if (matches[m].isWinMatch === false) {
-						if (isFreePlace(m)) {
-							var playerIndex = matches[m].players[0] == undefined ? 0 : 1;
-							matches[m].players[playerIndex] = loser;
-							makeBtn(loser.name, m, playerIndex);
-							break;
-						}
-					}
-				}
-			}
-		}
+		var gridMatch = howManyAlive() < 3;
+		if (loser.losses < 2) movePlayer(gridMatch, loser); // move loser to (true) winners in superfinal or to (false) losers
 		if (winner.losses > 0) {
-			if (howManyAlive() < 3) {
-				// move winner of losers to winners
-				for (var m = 0; m < matches.length; m++) {
-					if (matches[m].isWinMatch === true) {
-						if (isFreePlace(m)) {
-							var playerIndex = matches[m].players[0] == undefined ? 0 : 1;
-							matches[m].players[playerIndex] = winner;
-							makeBtn(winner.name, m, playerIndex);	
-							break;
-						}
-					}
-				}
-			} else {
-				// move earlier loser to losers
-				for (var m = 0; m < matches.length; m++) {
-					setMatchType(m, false);
-					if (matches[m].isWinMatch === false) {
-						if (isFreePlace(m)) {
-							var playerIndex = matches[m].players[0] == undefined ? 0 : 1;
-							matches[m].players[playerIndex] = winner;
-							makeBtn(winner.name, m, playerIndex);
-							break;
-						}
-					}
-				}
-			}
+			movePlayer(gridMatch, winner); // move winner of losers to (true) winners or to (false) losers
 		} else {
-			// move winner to winners
-			for (var m = 0; m < matches.length; m++) {
-				setMatchType(m, true);
-				if (matches[m].isWinMatch === true) {
-					if (isFreePlace(m)) {
-						var playerIndex = matches[m].players[0] == undefined ? 0 : 1;
-						matches[m].players[playerIndex] = winner;
-						makeBtn(winner.name, m, playerIndex);	
-						break;
-					}
-				}
-			}
+			movePlayer(true, winner); // move winner to winners
 		}
 	}
 }
